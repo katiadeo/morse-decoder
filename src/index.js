7666880196b7b561ed35b117068729e6fa690802
katiadeo <niel.0305@yandex.ru>
2021-07-05 18:35:50 +0300

    feat: start morse-decoder task

--- src/index.js
+++ src/index.js
@@ -37,8 +37,33 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+// function for string separation by n-size characters
+function separateStr(str, size) {
+    return str.length ? [str.slice(0, size), ...separateStr(str.slice(size), size)] : [];
+}
+
+// function for decoding
 function decode(expr) {
-    // write your solution here
+    const arrOfTen = separateStr(expr, 10);
+    let char;
+    
+    const arrOfSymbols = arrOfTen.map(ten => {
+        char = '';
+        if (ten.includes('*')) return char = ' ';
+        for (let i = 0; i < ten.length; i += 2) {
+            ten[i] === '1' && ten[i + 1] === '1'
+            ? char += '-' : ten[i] === '1' && ten[i + 1] === '0'
+            ? char += '.' : '';
+        }
+        return char;
+    });
+    
+    let decoded = arrOfSymbols.map(key => {
+        if (key === ' ') return ' ';
+        if (key in MORSE_TABLE) return MORSE_TABLE[key];
+    }).join('');
+    
+    return decoded;
 }
 
 module.exports = {
